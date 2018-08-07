74a000dd7e9a71f29fc7be3fac0ed646ff78544e
kamal11 <kyzx11@gmail.com>
2018-08-07 14:27:34 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./src/reducers";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Hello!</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
